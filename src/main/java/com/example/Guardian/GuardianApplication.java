118cdaaabfce00ce4764800c026b478d4b802d99
CodingWithCuba <mrmukhuba@gmail.com>
2026-05-27 14:07:34 +0200

    Enable scheduling and JPA auditing in application

--- src/main/java/com/example/Guardian/GuardianApplication.java
+++ src/main/java/com/example/Guardian/GuardianApplication.java
@@ -2,8 +2,14 @@ package com.example.Guardian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
 
 @SpringBootApplication
+@EnableScheduling
+@EnableJpaAuditing
 public class GuardianApplication {
 
 	public static void main(String[] args) {
