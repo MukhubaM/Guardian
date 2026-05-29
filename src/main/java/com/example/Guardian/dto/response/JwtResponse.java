373dd76fb59205f1f1e3eedec43b53453cd9a9ca
CodingWithCuba <mulwelim37@gmail.com>
2026-05-29 10:51:51 +0200

    dto response files commits

--- src/main/java/com/example/Guardian/dto/response/JwtResponse.java
+++ src/main/java/com/example/Guardian/dto/response/JwtResponse.java
@@ -1,4 +1,23 @@
 package com.example.Guardian.dto.response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class JwtResponse {
+
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType = "Bearer";
+    private long expiresIn;
+    private String role;
+    private String email;
+    private String username;
+
 }
--- src/main/java/com/example/Guardian/dto/response/UserResponse.java
+++ src/main/java/com/example/Guardian/dto/response/UserResponse.java
@@ -1,4 +1,29 @@
 package com.example.Guardian.dto.response;
 
+
+import com.example.Guardian.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponse {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String profilePicture;
+    private Role role;
+    private boolean enabled;
+    private boolean emailVarified;
+    private LocalDateTime createdAt;
 }
